5e0a330272534f64eafb35479097ef3b9472f2db
sinnyunki <47134435+sinnyunki@users.noreply.github.com>
2026-05-23 16:59:38 +0900

    ㅋ

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const albumsPath = path.join(root, 'albums.json');
 const types = {
   '.html': 'text/html; charset=utf-8',
   '.json': 'application/json; charset=utf-8',
+  '.webmanifest': 'application/manifest+json; charset=utf-8',
   '.js': 'text/javascript; charset=utf-8',
   '.css': 'text/css; charset=utf-8',
   '.svg': 'image/svg+xml',
